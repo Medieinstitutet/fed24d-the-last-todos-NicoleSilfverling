693815f5ff5ccf0fdb1b90767600510aa9d713df
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2025-06-06 01:34:23 +0200

    feat: display overdue waring

--- src/components/TodoPresentation.tsx
+++ src/components/TodoPresentation.tsx
@@ -10,14 +10,21 @@ export const TodoPresentation = ({
   deleteTodo,
   toggleIsCompleted,
 }: TodoPresentationProps) => {
+  const isOverdue = !todo.isCompleted && todo.dueDate < new Date();
   return (
-    <div className="bg-white shadow-md rounded-md p-4 mb-4 flex items-center justify-between">
+    <div className="relative bg-white shadow-md rounded-md p-4 mb-4 flex items-center justify-between">
+      {isOverdue && (
+        <p className="absolute top-2 right-2 text-red-600 font-semibold text-sm">
+          ⚠️ Overdue!
+        </p>
+      )}
       <div>
         <h3 className="text-lg font-semibold text-gray-800">{todo.title}</h3>
         <p className="text-gray-600">{todo.description}</p>
         <p className="text-sm text-gray-500">
           Due: {todo.dueDate.toLocaleDateString()}
         </p>
+
         <button
           onClick={() => {
             deleteTodo(todo.id);
